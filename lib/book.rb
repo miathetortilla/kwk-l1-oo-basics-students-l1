c4e6f8e4558d8967643dbf4e77bfc5402bd16e09
miathetortilla <mia.lamontagne@gmail.com>
2018-06-07 22:23:03 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,13 +8,14 @@ class Book
   end
   
 
-  def turn_page
+def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 end
 
 book_one = Book.new("And Then There Were None")
 book = Book.new("Some Title")
+
 book.turn_page
 
-book_one
+book_one.turn_page
