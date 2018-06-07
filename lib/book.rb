b15051f66a496aa8c955b9bfdcee3b5462a09714
miathetortilla <mia.lamontagne@gmail.com>
2018-06-07 22:22:16 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
-# book.rb
-# Add you Book class here
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+book_one = Book.new("And Then There Were None")
+book = Book.new("Some Title")
+book.turn_page
+
+book_one
