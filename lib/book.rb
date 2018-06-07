42027e795f7254dc8070f5e84b2a7898ab5b8219
miathetortilla <mia.lamontagne@gmail.com>
2018-06-07 22:31:31 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
-# book.rb
-# Add you Book class here
+class Book
+  
+  def initialize(title, author, turn_page = "Flipping the page...wow, you read fast!")
+    @title = title
+    @author = author
+    @turn_page = turn_page
+  end
+  
+  def title
+    @title
+  end
+  
+  def author
+    @author
+  end
+
+  def turn_page
+  @turn_page
+  end
+end
+
+
+book = Book.new("And Then There Were None","Agatha Christie")
+
+
+book.title
+book.author
+book.turn_page
+
