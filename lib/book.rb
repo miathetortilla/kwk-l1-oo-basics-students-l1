e8b132a0a1082d9984ce862ebe805a13bcd740bc
miathetortilla <mia.lamontagne@gmail.com>
2018-06-07 22:26:48 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
-class Book(title, author)
-  def initialize(title)
+class Book
+  def initialize(title, author)
     @title = title
   end
   
@@ -24,33 +24,3 @@ book.turn_page
 book.author
 book.title
 
-class Cars
-  def initialize(make, model, color = "white")
-    @make = make
-    @model = model
-    @color = color
-  end
-
-  def make
-    @make
-  end
-
-  def model
-    @model
-  end
-
-  def color
-    @color
-  end
-
-  def horn
-    return "BEEP!!!"
-  end
-
-  end
-
-car_one = Cars.new("Toyota", "Camry" , "Red")
-puts car_one.horn
-puts car_one.make
-puts car_one.model
-puts car_one.color
